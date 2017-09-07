c735a48a77e001cace0faa9f5b3b371b589db5e2
yupengpeng <18237567315@163.com>
2017-09-07 20:23:45 +0800

    1.0

--- src/main/java/com/example/demo/JavaPublicDemoApplication.java
+++ src/main/java/com/example/demo/JavaPublicDemoApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JavaPublicDemoApplication {
 
 	public static void main(String[] args) {
+
+
 		SpringApplication.run(JavaPublicDemoApplication.class, args);
 	}
 }
