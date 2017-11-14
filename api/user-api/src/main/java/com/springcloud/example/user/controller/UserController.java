44cfc65120714aa38ed43f135caf42174cfb9200
devin <songdaqing@51lianchuan.com>
2017-11-14 13:32:52 +0800

    修改提交

--- api/user-api/src/main/java/com/springcloud/example/user/controller/UserController.java
+++ api/user-api/src/main/java/com/springcloud/example/user/controller/UserController.java
@@ -1,9 +1,11 @@
 package com.springcloud.example.user.controller;
 
 
+import com.springcloud.example.user.config.AppConfig;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,10 +22,15 @@ public class UserController {
 
     private static final Logger logger = Logger.getLogger(UserController.class);
 
+    @Autowired
+    private AppConfig appConfig;
+
     @ApiOperation(value="用户", notes="示例，获取用户")
     @RequestMapping(value = "/getUser" ,method = RequestMethod.POST)
-    public void getUser()  throws Exception {
+    public String getUser()  throws Exception {
         logger.info("访问用户成功");
+        return appConfig.getAppName();
+
     }
 
 }
