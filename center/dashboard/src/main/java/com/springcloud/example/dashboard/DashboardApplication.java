07ba7e984d17f55a14187114e2a0b4e99d0a969a
devin <songdaqing@51lianchuan.com>
2017-11-30 10:33:23 +0800

    Tijao

--- center/dashboard/src/main/java/com/springcloud/example/dashboard/DashboardApplication.java
+++ center/dashboard/src/main/java/com/springcloud/example/dashboard/DashboardApplication.java
@@ -4,10 +4,8 @@ import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import zipkin.server.EnableZipkinServer;
 
 
-@EnableZipkinServer
 @EnableDiscoveryClient
 @SpringBootApplication
 public class DashboardApplication {
